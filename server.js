875778df993e642a4d60841fe67338ffe0fccf05
syuz-poghosyan <81686567+syuz-poghosyan@users.noreply.github.com>
2021-03-31 15:01:47 +0400

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { MongoClient } = require("mongodb");
 const ObjectId = require('mongodb').ObjectId; 
 
 const uri =
-  "mongodb+srv://cse120-2021-user:aua-gened-cse120@cse120-2021.zmhgf.mongodb.net/test";
+  mongodb+srv://Syuzanna-admin:MongodbSyuz2021@cluster0.hzwax.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 
 const client = new MongoClient(uri);
 

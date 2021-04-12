7aad0b2bd9a34ed7cfe20e1ef546b89d58686c0f
syuz-poghosyan <81686567+syuz-poghosyan@users.noreply.github.com>
2021-04-12 13:08:25 +0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -526,5 +526,5 @@ function loadDanceEditItem(){
 }
 
 function goBack() {
-  window.location.href = "https://cse-120-2021-api-syuzi.herokuapp.com/admin.html";
+  window.location.href = "https://cse-120-2021-api-syuzi.herokuapp.com";
 }
